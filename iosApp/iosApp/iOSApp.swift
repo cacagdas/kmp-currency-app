56415e29958c1d282a3438e4690c7cff81c3c9b9
alicagdascolak <alicagdas.colak@vektortelekom.com>
2024-07-23 17:08:56 +0300

    fix koin injection for platforms

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,12 @@
 import SwiftUI
+import ComposeApp
 
 @main
 struct iOSApp: App {
+
+    init() {
+        KoinModuleKt.initKoin()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
